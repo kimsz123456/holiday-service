bb5222221eded9ac4cf6309f89bf1f4f1f90d777
jiseung <kimsz123456@gmail.com>
2025-12-03 11:34:38 +0900

    chore: 프로젝트 빌드

---
+++ src/main/java/org/example/HolidayServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayServiceApplication.class, args);
+    }
+}
