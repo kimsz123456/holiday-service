6ed120f11219df8b670b4f7a6ba82c5166dcc991
jiseung <kimsz123456@gmail.com>
2025-12-04 21:11:12 +0900

    fix: 에러 핸들러 추가

--- src/main/java/com/holiday/exception/RestTemplateErrorHandler.java
+++ src/main/java/com/holiday/exception/RestTemplateErrorHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
